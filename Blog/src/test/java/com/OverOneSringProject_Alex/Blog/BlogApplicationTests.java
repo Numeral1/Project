2c3ab22c553bf0f9568e7847760d749ca86c9c4d
Alex Che <sat.git333@mail.ru>
2021-03-02 09:56:22 +0300

    first

---
+++ Blog/src/test/java/com/OverOneSringProject_Alex/Blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OverOneSringProject_Alex.Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
