2d8a40361bd0ec31481a5b05023b0d1b3d9111a5
Alex Che <sat.git333@mail.ru>
2021-07-27 14:11:06 +0300

    the DB is created then the application starts

---
+++ com.example/src/main/java/com/example/spring/domain/Role.java
@@ -0,0 +1,12 @@
+package com.example.spring.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
